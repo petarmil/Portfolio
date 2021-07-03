6907f94e80711059b5ad4a5053909ac9e8c8e1b7
petarmil <62208419+petarmil@users.noreply.github.com>
2021-07-03 14:03:21 +0200

    Add files via upload

---
+++ animation.js
@@ -0,0 +1,243 @@
+var skills = $('.skill');
+var work = $('.work');
+var headerContact = $('.header_contact');
+var headerSocial = $('.header_social');
+
+var $window = $(window);
+
+function skillsAnimation() {
+    var window_height = $window.height();
+    var window_top_position = $window.scrollTop();
+    var window_bottom_position = (window_top_position + window_height);
+
+    $.each(skills, function () {
+        var $element = $(this);
+        var element_height = $element.outerHeight();
+        var element_top_position = $element.offset().top;
+        var element_bottom_position = (element_top_position + element_height);
+
+
+
+
+        if ((element_bottom_position >= window_top_position) &&
+            (element_top_position <= window_bottom_position)) {
+            $element.addClass('animate__animated animate__backInLeft');
+        } else {
+            $element.removeClass('animate__animated animate__backInLeft');
+        }
+
+
+    });
+}
+
+function workAnimation() {
+    var window_height = $window.height();
+    var window_top_position = $window.scrollTop();
+    var window_bottom_position = (window_top_position + window_height);
+
+    $.each(work, function () {
+        var $element = $(this);
+        var element_height = $element.outerHeight();
+        var element_top_position = $element.offset().top;
+        var element_bottom_position = (element_top_position + element_height);
+
+
+
+
+        if ((element_bottom_position >= window_top_position) &&
+            (element_top_position <= window_bottom_position)) {
+            $element.addClass('animate__animated animate__heartBeat');
+        } else {
+            $element.removeClass('animate__animated animate__heartBeat');
+        }
+
+
+    });
+}
+
+function headerContactAnimation() {
+    var window_height = $window.height();
+    var window_top_position = $window.scrollTop();
+    var window_bottom_position = (window_top_position + window_height);
+
+    $.each(headerContact, function () {
+        var $element = $(this);
+        var element_height = $element.outerHeight();
+        var element_top_position = $element.offset().top;
+        var element_bottom_position = (element_top_position + element_height);
+
+
+
+
+        if ((element_bottom_position >= window_top_position) &&
+            (element_top_position <= window_bottom_position)) {
+            $element.addClass('animate__animated animate__fadeInLeft');
+        } else {
+            $element.removeClass('animate__animated animate__fadeInLeft');
+        }
+
+
+    });
+}
+
+function headerSocialAnimation() {
+    var window_height = $window.height();
+    var window_top_position = $window.scrollTop();
+    var window_bottom_position = (window_top_position + window_height);
+
+    $.each(headerSocial, function () {
+        var $element = $(this);
+        var element_height = $element.outerHeight();
+        var element_top_position = $element.offset().top;
+        var element_bottom_position = (element_top_position + element_height);
+
+
+
+
+        if ((element_bottom_position >= window_top_position) &&
+            (element_top_position <= window_bottom_position)) {
+            $element.addClass('animate__animated animate__fadeInRight');
+        } else {
+            $element.removeClass('animate__animated animate__fadeInRight');
+        }
+
+
+    });
+}
+
+function headerNavAnimation() {
+    var window_height = $window.height();
+    var window_top_position = $window.scrollTop();
+    var window_bottom_position = (window_top_position + window_height);
+
+    $.each($('.header_nav'), function () {
+        var $element = $(this);
+        var element_height = $element.outerHeight();
+        var element_top_position = $element.offset().top;
+        var element_bottom_position = (element_top_position + element_height);
+
+
+
+
+        if ((element_bottom_position >= window_top_position) &&
+            (element_top_position <= window_bottom_position)) {
+            $element.addClass('animate__animated animate__fadeInDown');
+        } else {
+            $element.removeClass('animate__animated animate__fadeInDown');
+        }
+
+
+    });
+}
+
+function hamburgerAnimation() {
+    var window_height = $window.height();
+    var window_top_position = $window.scrollTop();
+    var window_bottom_position = (window_top_position + window_height);
+
+    $.each($('.hamburger'), function () {
+        var $element = $(this);
+        var element_height = $element.outerHeight();
+        var element_top_position = $element.offset().top;
+        var element_bottom_position = (element_top_position + element_height);
+
+
+
+
+        if ((element_bottom_position >= window_top_position) &&
+            (element_top_position <= window_bottom_position)) {
+            $element.addClass('animate__animated animate__rotateIn');
+        } else {
+            $element.removeClass('animate__animated animate__rotateIn');
+        }
+
+
+    });
+}
+
+function headingAnimation() {
+    var window_height = $window.height();
+    var window_top_position = $window.scrollTop();
+    var window_bottom_position = (window_top_position + window_height);
+
+    $.each($('h2'), function () {
+        var $element = $(this);
+        var element_height = $element.outerHeight();
+        var element_top_position = $element.offset().top;
+        var element_bottom_position = (element_top_position + element_height);
+
+
+
+
+        if ((element_bottom_position >= window_top_position) &&
+            (element_top_position <= window_bottom_position)) {
+            $element.addClass('animate__animated animate__zoomIn');
+        } else {
+            $element.removeClass('animate__animated animate__zoomIn');
+        }
+
+
+    });
+}
+
+function paragraphAnimation() {
+    var window_height = $window.height();
+    var window_top_position = $window.scrollTop();
+    var window_bottom_position = (window_top_position + window_height);
+
+    $.each($('p'), function () {
+        var $element = $(this);
+        var element_height = $element.outerHeight();
+        var element_top_position = $element.offset().top;
+        var element_bottom_position = (element_top_position + element_height);
+
+
+
+
+        if ((element_bottom_position >= window_top_position) &&
+            (element_top_position <= window_bottom_position)) {
+            $element.addClass('animate__animated animate__fadeIn');
+        } else {
+            $element.removeClass('animate__animated animate__fadeIn');
+        }
+
+
+    });
+}
+
+function mobileMenuAnimation() {
+    var window_height = $window.height();
+    var window_top_position = $window.scrollTop();
+    var window_bottom_position = (window_top_position + window_height);
+
+    $.each($('.mobile-menu'), function () {
+        var $element = $(this);
+        var element_height = $element.outerHeight();
+        var element_top_position = $element.offset().top;
+        var element_bottom_position = (element_top_position + element_height);
+
+
+
+
+        if ((element_bottom_position >= window_top_position) &&
+            (element_top_position <= window_bottom_position)) {
+            $element.addClass('animate__animated animate__fadeIn');
+        } else {
+            $element.removeClass('animate__animated animate__fadeIn');
+        }
+
+
+    });
+}
+
+
+$window.on('scroll resize', skillsAnimation);
+$window.on('scroll resize', workAnimation);
+$window.on('scroll resize', headerContactAnimation);
+$window.on('scroll resize', headerSocialAnimation);
+$window.on('scroll resize', headerNavAnimation);
+$window.on('scroll resize', hamburgerAnimation);
+//$window.on('scroll resize', headingAnimation);
+//$window.on('scroll resize', paragraphAnimation);
+$window.on('scroll resize', mobileMenuAnimation);
+$window.trigger('scroll');
